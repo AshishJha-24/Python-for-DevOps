1093e9160cc10336634bded4bb262bc56fa6ce21
AshishJha-24 <aj230104@gmail.com>
2024-08-26 18:36:51 +0530

    understand about operators

---
+++ Day06/question1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+sum = a+b
+diff = a-b 
+mul = a*b
+div = a/b
+remainder = a%b
+
+print ("sum is ", sum)
+print ("diff is ", diff)
+print ("mul is ", mul)
+print ("div is ", div)
+print ("remainder is ", remainder)
